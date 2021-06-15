ac529219742e26a702f806c0d779454e01a1a0ca
minhdat1602 <18130035@st.hcmuaf.edu.vn>
2021-06-15 21:09:22 +0700

    .

--- src/main/java/com/nlu/config/CharacterEncodingConfig.java
+++ src/main/java/com/nlu/config/CharacterEncodingConfig.java
@@ -1,11 +1,14 @@
 package com.nlu.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
 public class CharacterEncodingConfig {
-//	@Bean
-//	CharacterEncodingFilter characterEncodingFilter() {
-//		CharacterEncodingFilter filter = new CharacterEncodingFilter();
-//		filter.setEncoding("UTF-8");
-//		filter.setForceEncoding(true);
-//		return filter;
-//	}
+	@Bean
+	CharacterEncodingFilter characterEncodingFilter() {
+		CharacterEncodingFilter filter = new CharacterEncodingFilter();
+		filter.setEncoding("utf-8");
+		filter.setForceEncoding(true);
+		return filter;
+	}
 }
