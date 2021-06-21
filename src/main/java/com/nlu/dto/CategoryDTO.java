6714bb634525024f831c2316c1e5fc6f0b15343e
minhdat1602 <18130035@st.hcmuaf.edu.vn>
2021-06-21 14:40:51 +0700

    .

--- src/main/java/com/nlu/dto/CategoryDTO.java
+++ src/main/java/com/nlu/dto/CategoryDTO.java
@@ -4,20 +4,20 @@ import java.util.Set;
 
 public class CategoryDTO  extends BaseDTO{
 
-	private Set<ProductDTO> products;
+	
 
 	private String code;
 
 	private String name;
 
 
-	public Set<ProductDTO> getProducts() {
-		return products;
-	}
-
-	public void setProducts(Set<ProductDTO> products) {
-		this.products = products;
-	}
+//	public Set<ProductDTO> getProducts() {
+//		return products;
+//	}
+//
+//	public void setProducts(Set<ProductDTO> products) {
+//		this.products = products;
+//	}
 
 	public String getCode() {
 		return code;
