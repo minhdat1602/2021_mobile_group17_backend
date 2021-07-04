e4c771cc7375861f8c214bd350a9f0b1fc2697e4
minhdat1602 <18130035@st.hcmuaf.edu.vn>
2021-07-04 15:28:46 +0700

    .

--- src/main/java/com/nlu/controller/OrderController.java
+++ src/main/java/com/nlu/controller/OrderController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +38,12 @@ public class OrderController {
 
 	@PostMapping()
 	public ResponseEntity<?> save(@RequestBody OrderDTO orderDTO) {
+		OrderDTO order = orderService.save(orderDTO);
+		return ResponseEntity.ok(order);
+	}
+	
+	@PutMapping()
+	public ResponseEntity<?> update(@RequestBody OrderDTO orderDTO) {
 		OrderDTO order = orderService.save(orderDTO);
 		return ResponseEntity.ok(order);
 	}
