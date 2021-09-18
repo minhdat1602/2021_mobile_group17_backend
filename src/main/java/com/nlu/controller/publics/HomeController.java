5f2cfa2ddcf0b7850604cbead4aa39d3f6386977
minhdatnlu <18130035@st.hcmuaf.edu.vn>
2021-09-18 11:56:59 +0700

    .

--- src/main/java/com/nlu/controller/publics/HomeController.java
+++ src/main/java/com/nlu/controller/publics/HomeController.java
@@ -3,6 +3,7 @@ package com.nlu.controller.publics;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ public class HomeController {
 		return ResponseEntity.ok(homeService.findByIsHot());
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/highlight")
 	public ResponseEntity<?> fetchByIsHighLight(){
 		return ResponseEntity.ok(homeService.findByIsHighLight());
